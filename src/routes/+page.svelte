01b874a6a9a11928815c7eb038696a4b2fc3e9f3
Kacper <adamczyk911119@gmail.com>
2022-12-03 16:22:47 +0000

    Reintegrated nav, login, register, and authentication pages from tempaltes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,65 @@
-
-
 <script>
-	import Modal from '../components/Modal.svelte'
-	let showLoginModal = false
-	
+	// import Modal from '../components/Modal.svelte';
+	// let showLoginModal = false;
 </script>
 
-
-
-
-{#if showLoginModal}
-	<Modal on:close="{() => showLoginModal = false}">
-		<h1 slot="header" style="text-align:center">
-			Login
-		</h1>
-        <center>
-		<form class="w-full max-w-sm" style="margin-top:1em;">
-            
-                <input style="margin-bottom:1em;background-color:white;border:none;" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="email" placeholder="Email">
-                <input style="background-color:white;border:none;" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="Password">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style="background-color:#5C728A;margin-top:0.5em;margin-bottom:1em;">
-                Login
-                </button>
-             
-          </form>
-	</Modal>
-{/if}
-
+<!-- {#if showLoginModal}
+	<Modal on:close={() => (showLoginModal = false)}>
+		<h1 slot="header" style="text-align:center">Login</h1>
+		<center>
+			<form class="w-full max-w-sm" style="margin-top:1em;">
+				<input
+					style="margin-bottom:1em;background-color:white;border:none;"
+					class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+					id="inline-full-name"
+					type="email"
+					placeholder="Email"
+				/>
+				<input
+					style="background-color:white;border:none;"
+					class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+					id="inline-password"
+					type="password"
+					placeholder="Password"
+				/>
+				<button
+					class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+					style="background-color:#5C728A;margin-top:0.5em;margin-bottom:1em;"
+				>
+					Login
+				</button>
+			</form>
+		</center></Modal
+	>
+{/if} -->
 
 <center>
-<div id="buttons" style="">
-    <img src="src/public/logo.png" style="height:45em;" alt="logo"/>
+	<div id="buttons" style="">
+		<img src="src/public/logo.png" style="height:45em;" alt="logo" />
 
-<!-- Using utilities: -->
-<!-- Simplest use: modal without an `id` or callback function -->
-<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style="background-color:#5C728A;display:block" on:click="{() => showLoginModal = true}">
+		<!-- Using utilities: -->
+		<!-- Simplest use: modal without an `id` or callback function -->
+		<!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style="background-color:#5C728A;display:block" on:click="{() => showLoginModal = true}">
     Login
-  </button>
-  
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style="background-color:#5C728A;margin-top:0.5em;">
-    Sign up
-  </button>
-  
- </div>
+  </button> -->
+		<a href="/login" >
+			<button
+				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				style="background-color:#5C728A;display:block"
+			>
+				Login
+			</button>
+		</a>
+
+    <a href="/register">
+			<button
+				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				style="background-color:#5C728A;display:block;margin-top:0.5em;"
+			>
+				Sign up
+			</button>
+		</a>
+
+		
+	</div>
 </center>
-
-
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,39 @@
+<center>
+	<form style="width:30%;margin-top:10em;" method="post">
+		<div class="mb-6">
+			<input
+				name="email"
+				type="email"
+				id="email"
+				class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+				placeholder="Email"
+				required
+			/>
+		</div>
+		<div class="mb-6">
+			<input
+				name="password"
+				type="password"
+				id="password"
+				class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+				placeholder="Password"
+				required
+			/>
+		</div>
+		<input
+			type="submit"
+			value="Login"
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			style="background-color:#5C728A;margin-top:0.5em;cursor:pointer;"
+		/>
+	</form>
+</center>
+
+<style>
+    #email, #password{
+        background-color: white;
+        border:0;
+        color: black;
+    }
+</style>
+
