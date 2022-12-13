e8044c9c1420cad9453d21d4d4f2ed837899b830
conor <conorbegley5@gmail.com>
2022-12-13 20:43:06 +0000

    fixed a small bug found, this bug was that the input file wasn't accepting files.

--- src/routes/book/+page.svelte
+++ src/routes/book/+page.svelte
@@ -104,12 +104,12 @@
             <div class="chan" on:click={()=>{fileinput.click();}}>Choose Video</div>
 			<input
 				name="movie_url"
-				type="text"
+				type="file"
 				id="movie_url"
 				class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
 				placeholder="Movie URL"
                 bind:value={bookingInfo.movie_url}
-                type="file"  on:change={(e)=>onFileSelected(e)} bind:this={fileinput}
+                on:change={(e)=>onFileSelected(e)} bind:this={fileinput}
 				required
 			/>
 		</div>
