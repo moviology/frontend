a015fff0a39fc286c9274f7e60b6ff23baf08149
conor <conorbegley5@gmail.com>
2022-12-13 20:52:21 +0000

    add validation so the user can only upload a mp4 file

--- src/routes/book/+page.svelte
+++ src/routes/book/+page.svelte
@@ -108,6 +108,7 @@
 				id="movie_url"
 				class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
 				placeholder="Movie URL"
+                accept=".mp4"
                 bind:value={bookingInfo.movie_url}
                 on:change={(e)=>onFileSelected(e)} bind:this={fileinput}
 				required
